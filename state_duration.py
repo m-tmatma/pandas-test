3f7a97846b41eef53cf5232b59b4c850fdd49642
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-11-28 17:36:49 +0900

    histram の範囲を修正

--- state_duration.py
+++ state_duration.py
@@ -41,7 +41,10 @@ for i in range(num):
 
     df.to_csv("df_" + str(i) + ".csv")
 
-    ax.hist(df['duration'].values, bins=range(6), label=str(i), color=cm.colors[i])
+    min_value=int(np.trunc(df['duration'].min()))
+    max_value=int(np.floor(df['duration'].max())) + 1
+
+    ax.hist(df['duration'].values, bins=range(min_value, max_value+1), label=str(i), color=cm.colors[i])
     ax.legend()
 
 fig.savefig("img.png")
