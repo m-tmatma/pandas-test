65ef9949e915e6bf46a5b41788b4cec71d3ed2cd
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-10-07 10:04:10 +0900

    add state change

---
+++ state_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+''''
+指定した値の変化点を調べるテストスクリプト
+'''
+import numpy as np
+import pandas as pd
+
+num   = 4
+
+state = np.random.randint(num, size=100)
+x     = np.arange(100)
+y     = np.arange(100,200)
+
+df = pd.DataFrame( {'x' : x, 'y' : y, 'state': state })
+
+states = df['state']
+
+df['change'] = states != states.shift()
+dfChange = df[ df['change'] ]
+
+df.to_csv("df.csv")
+dfChange.to_csv("df_change.csv")
