8dbebcc479c4826942efcb04c21acd6496a688aa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-17 13:57:28 +0900

    fix warning

--- timestamp-df.py
+++ timestamp-df.py
@@ -4,13 +4,17 @@
 #   Prepare
 #       pip install pandas openpyxl numpy
 #
+# See https://towardsdatascience.com/how-to-auto-adjust-the-width-of-excel-columns-with-pandas-excelwriter-60cee36e175e
 import pandas as pd
 import numpy as np
 import datetime
 
+def timedata(i):
+    return datetime.datetime.now() + datetime.timedelta(microseconds=i*1001*1001)
+
 df = pd.DataFrame(
     {
-        'timestamp': [ datetime.datetime.now() + datetime.timedelta(microseconds=i*1001*1001) for i in range(10)],
+        'timestamp': [ timedata(i) for i in range(10)],
         'random': np.random.random(10),
     }
 )
@@ -20,7 +24,6 @@ with pd.ExcelWriter('timestamp-test.xlsx') as writer:
     sheet_name='Test'
     df.to_excel(writer, sheet_name=sheet_name, index=False)
 
-    # See https://towardsdatascience.com/how-to-auto-adjust-the-width-of-excel-columns-with-pandas-excelwriter-60cee36e175e
     for column in df:
         column_width = max(df[column].astype(str).map(len).max(), len(column))
         col_idx = df.columns.get_loc(column)
