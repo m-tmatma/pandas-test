aec8829347cc4dd34f2a1c7e2365d6376da94f4d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-17 13:48:50 +0900

    auto adjust

--- timestamp-df.py
+++ timestamp-df.py
@@ -15,4 +15,10 @@ df = pd.DataFrame(
 df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
 
 with pd.ExcelWriter('timestamp-test.xlsx') as writer:
-    df.to_excel(writer, sheet_name='Test')
+    df.to_excel(writer, sheet_name='Test', index=False)
+
+    # See https://towardsdatascience.com/how-to-auto-adjust-the-width-of-excel-columns-with-pandas-excelwriter-60cee36e175e
+    for column in df:
+        column_width = max(df[column].astype(str).map(len).max(), len(column))
+        col_idx = df.columns.get_loc(column)
+        writer.sheets['Test'].set_column(col_idx, col_idx, column_width)
