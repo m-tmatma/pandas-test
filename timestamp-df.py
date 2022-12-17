43fc2994b21e91a4bea651b1eb248a676310b551
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-12-17 13:48:01 +0900

    tab to spaces

--- timestamp-df.py
+++ timestamp-df.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 #
 #
-#	Prepare
-#		pip install pandas openpyxl
+#   Prepare
+#       pip install pandas openpyxl
 #
 import pandas as pd
 import datetime
 
 df = pd.DataFrame(
-	{
-		'timestamp': [ datetime.datetime.now() + datetime.timedelta(microseconds=i*1001*1001) for i in range(10)]
-	}
+    {
+        'timestamp': [ datetime.datetime.now() + datetime.timedelta(microseconds=i*1001*1001) for i in range(10)]
+    }
 )
 df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
 
 with pd.ExcelWriter('timestamp-test.xlsx') as writer:
-	df.to_excel(writer, sheet_name='Test')
+    df.to_excel(writer, sheet_name='Test')
