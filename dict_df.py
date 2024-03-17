c410e46e7ff692c21bd0057f7d57bc6215486ea8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-03-17 19:05:10 +0900

    fix

--- dict_df.py
+++ dict_df.py
@@ -6,10 +6,11 @@ data = [
     {"name": "y", "value": "y"},
     {"name": "state", "value": "state"},
     {"name": "change", "value": "change"},
-    {"name": "duration", "value": "duration"}
+    {"name": "duration", "value": "duration"},
+    {"test": "test3", "value": "test4"},
+    {"hoge": "test3", "hoge2": "test4"},
 ]
 
 df = pd.DataFrame(data)
 print(df)
-
 
