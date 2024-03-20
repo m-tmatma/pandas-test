8e4f29869d0d6dd1a12df84462f2f97178faf635
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-03-20 11:54:39 +0900

    np.polyval の方法を追加

--- pandas_polyfit.py
+++ pandas_polyfit.py
@@ -15,16 +15,24 @@ df = pd.DataFrame({
 coeff = np.polyfit(df['x'], df['y'], 1)
 vfunc = np.vectorize(lambda x: coeff[0] * x + coeff[1])
 
+# やり方1
 start = time.time()
 df["fit"]  = df['x'].apply(vfunc)
 diff1  = time.time() - start
 
+# やり方2
 start = time.time()
 df["fit2"]  = vfunc(df['x'])
 diff2  = time.time() - start
+
+# やり方3
+start = time.time()
+df["fit3"]  = np.polyval(coeff, df['x'])
+diff3  = time.time() - start
 
 df["error"] = (df['fit'] - df['y']).abs()
 df["rate%"] = (df['error'] / df["y"]) * 100
 print(df)
 print(diff1)
 print(diff2)
+print(diff3)
