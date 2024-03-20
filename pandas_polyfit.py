3c78441e69b7c69b32b69f29f5c24804099d0c29
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-03-20 11:16:37 +0900

    別の方法を追加

--- pandas_polyfit.py
+++ pandas_polyfit.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+import time
 
 # 仮のデータフレームを作成
 start_x = 10
 start_y = 100
-num     = 150
+num     = 150000
 df = pd.DataFrame({
     'x': np.arange(start_x, start_x+num),
     'y': np.arange(start_y, start_y+num) + np.random.randn(num)
@@ -14,7 +15,16 @@ df = pd.DataFrame({
 coeff = np.polyfit(df['x'], df['y'], 1)
 vfunc = np.vectorize(lambda x: coeff[0] * x + coeff[1])
 
+start = time.time()
 df["fit"]  = df['x'].apply(vfunc)
+diff1  = time.time() - start
+
+start = time.time()
+df["fit2"]  = vfunc(df['x'])
+diff2  = time.time() - start
+
 df["error"] = (df['fit'] - df['y']).abs()
 df["rate%"] = (df['error'] / df["y"]) * 100
 print(df)
+print(diff1)
+print(diff2)
