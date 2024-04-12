951388f2df83621203806b6e2e12798274e62f09
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-04-13 08:38:15 +0900

    log

--- pandas_join.py
+++ pandas_join.py
@@ -5,17 +5,22 @@ df2 = pd.read_csv('data2.csv').set_index(['major_id', 'minor_id'])
 df3 = pd.read_csv('data3.csv').set_index(['major_id', 'minor_id'])
 df4 = pd.read_csv('data4.csv').set_index(['major_id', 'minor_id'])
 
+print("concat[df1, df2]")
 df = pd.concat([df1, df2], axis=1).reset_index()
 print(df)
 
+print("merge(df1, df2)")
 df = pd.merge(df1, df2)
 print(df)
 
+print("merge(df3, df4)")
 df = pd.concat([df3, df4], axis=1).reset_index()
 print(df)
 
+print("merge(df3, df4)")
 df = pd.merge(df3, df4)
 print(df)
 
+print("merge(df4, df3)")
 df = pd.merge(df4, df3)
 print(df)
