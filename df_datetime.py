b34e91ae9351dca9566f7ef78a8859e25e66a08f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-04-18 23:08:54 +0900

    pd.date_range

---
+++ df_datetime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        'Date': pd.date_range('2020-01-01', periods=10, freq='D'),
+    }
+)
+df['Date'] = pd.to_datetime(df['Date'])
+
+df2 = pd.DataFrame(
+    {
+        'Date': pd.date_range(df['Date'].max()+pd.offsets.Day(1), periods=10, freq='D'),
+    }
+)
+print(df)
+print(df2)
+df3 = pd.concat([df, df2], ignore_index=True)
+print(df3)
+
